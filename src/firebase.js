d5e9c8157a28a18339a49e7b9ec706399f96b4c3
andreyscott <62455784+andreyscott@users.noreply.github.com>
2021-04-11 20:42:06 +0100

    finally deploying my spotify clone

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBwEgaWIDiTQI5yWFg4MUuNKeIB_cgMTCc",
+    authDomain: "spotify-clone-17c62.firebaseapp.com",
+    projectId: "spotify-clone-17c62",
+    storageBucket: "spotify-clone-17c62.appspot.com",
+    messagingSenderId: "267286662524",
+    appId: "1:267286662524:web:5bf71e535e0810eaf6e8f9",
+    measurementId: "G-47K4N2E741"
+  };
